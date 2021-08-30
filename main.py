7a4116ccdb2fa8527dc79123920c4396462fd0fe
Your Name <your.email@example.com>
2021-08-30 23:06:09 +0900

    kageyama

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, abort
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -7,48 +6,59 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, VideoSendMessage, StickerSendMessage, AudioSendMessage
+    MessageEvent, TextMessage, TextSendMessage,
 )
 import os
-import random
+import wikipedia # 追加
 
 app = Flask(__name__)
 
-#環境変数取得
-LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+wikipedia.set_lang("ja") # 追加
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
-
 @handler.add(MessageEvent, message=TextMessage)
+# 先ほどの関数内にWikipediaのライブラリを使った処理を記述していく
 def handle_message(event):
-    # 基本的にここにコードを書いていきます。
-    message = event.message.text
+    send_message = event.message.text
+    # 正常に検索結果が返った場合
+    try:
+        wikipedia_page = wikipedia.page(send_message)
+        # wikipedia.page()の処理で、ページ情報が取得できれば、以下のようにタイトル、リンク、サマリーが取得できる。
+        wikipedia_title = wikipedia_page.title
+        wikipedia_url = wikipedia_page.url
+        wikipedia_summary = wikipedia.summary(send_message)
+        reply_message = '【' + wikipedia_title + '】\n' + wikipedia_summary + '\n\n' + '【詳しくはこちら】\n' + wikipedia_url
+    # ページが見つからなかった場合
+    except wikipedia.exceptions.PageError:
+        reply_message = '【' + send_message + '】\nについての情報は見つかりませんでした。'
+    # 曖昧さ回避にひっかかった場合
+    except wikipedia.exceptions.DisambiguationError as e:
+        disambiguation_list = e.options
+        reply_message = '複数の候補が返ってきました。以下の候補から、お探しの用語に近いものを再入力してください。\n\n'
+        for word in disambiguation_list:
+            reply_message += '・' + word + '\n'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=message))
-
+        TextSendMessage(reply_message)
+    )
 
 if __name__ == "__main__":
-#    app.run()
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
